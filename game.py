e67648d3a94603a8f203c86958c0705b4a5b115b
slafniy <slafniy@gmail.com>
2016-02-01 03:55:51 +0400

    First version with row cleaning

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ import enum
 import random
 import threading
 
+import time
+
 import custom_threads
 import figures
 
@@ -44,7 +46,7 @@ class Game:
         """
         self.width = width
         self.height = height
-        self.tick_interval = 0.2
+        self.tick_interval = 0.3
 
         # Functions to draw and remove cells
         # TODO: write interface for this
@@ -63,7 +65,7 @@ class Game:
         self._figure = None
         self.paused = False
 
-        self.tick_thread = custom_threads.TickThread(self.tick, self.tick_interval, game_over_event)
+        self.tick_thread = custom_threads.TickThread(self._tick, self.tick_interval, game_over_event)
         self.tick_thread.start()
 
         # To not allow simultaneous call of place()
@@ -83,24 +85,28 @@ class Game:
             elif state == CellState.FALLING:
                 cell.image_id = self._paint_ui_falling(x, y)
 
-    def fix_figure(self):
+    def _fix_figure(self):
         print("Fixing figure")
         for x, y in self._figure.current_points:
             self._set_cell_state(x, y, CellState.FILLED)
         self._refresh_ui()
         self._figure = None
 
-    def spawn_figure(self):
-        if self._figure is None:
+    def _spawn_figure(self):
+        # First, check full rows and clear them if needed
+        self._clear_rows()
+
+        # Spawn new if needed
+        if not self.game_over_event.is_set() and self._figure is None:
             figure_cls = random.choice(figures.all_figures)
             self._figure = figure_cls()
-            can_place = self.place()
+            can_place = self._place()
             if can_place is False:
                 self.game_over_event.set()
                 print('Cannot place new figure - game over')
             return can_place
 
-    def place(self, point=(4, 0)):
+    def _place(self, point=(4, 0)):
         self._is_busy = True
         try:
             target_points = set()
@@ -109,7 +115,7 @@ class Game:
                 y = _y + point[1]
                 if not (0 <= x < self.width and 0 <= y < self.height and
                         self._field[x][y].state != CellState.FILLED):
-                    print("Cannot place figure to {}".format(point))
+                    # print("Cannot place figure to {}".format(point))
                     return False
                 target_points.add((x, y))
             initial_points = copy.deepcopy(self._figure.current_points)
@@ -124,21 +130,21 @@ class Game:
 
             self._figure.position = point
             self._refresh_ui()
-            print("Figure placed to {}".format(point))
+            # print("Figure placed to {}".format(point))
             return True
         finally:
             self._is_busy = False
 
     def _move(self, x_diff=0, y_diff=0):
-        print("Trying to move...")
+        # print("Trying to move...")
         try:
             if self._is_busy:
-                print("place() method is busy, doing nothing!")
+                # print("place() method is busy, doing nothing!")
                 raise BusyWarning()
             if self._figure is None or self._figure.position is None:
-                print("There is no figure to move")
+                # print("There is no figure to move")
                 return False
-            return self.place((self._figure.position[0] + x_diff, self._figure.position[1] + y_diff))
+            return self._place((self._figure.position[0] + x_diff, self._figure.position[1] + y_diff))
         except BusyWarning:
             pass
 
@@ -155,29 +161,66 @@ class Game:
         pass
 
     def rotate(self):
-        if self._figure is not None:
+        if not self.game_over_event.is_set() and self._figure is not None:
             current_rotation = self._figure.rotation
             self._figure.set_next_rotation()
-            if not self.place(self._figure.position):
-                if not self.place((self._figure.position[0] - 1, self._figure.position[1])):
-                    if not self.place((self._figure.position[0], self._figure.position[1] - 1)):
-                        if not self.place((self._figure.position[0] - 1, self._figure.position[1] - 1)):
+            if not self._place(self._figure.position):
+                if not self._place((self._figure.position[0] - 1, self._figure.position[1])):
+                    if not self._place((self._figure.position[0], self._figure.position[1] - 1)):
+                        if not self._place((self._figure.position[0] - 1, self._figure.position[1] - 1)):
                             self._figure.rotation = current_rotation
 
-    def tick(self):
+    def _tick(self):
         if self.paused:
-            print("Skip tick because of pause")
+            # print("Skip tick because of pause")
             return
         if self.game_over_event.is_set():
-            print("Skip tick because of game over")
+            # print("Skip tick because of game over")
             return
 
         if self._figure is None:
             print("Trying to spawn new figure")
-            self.spawn_figure()
+            self._spawn_figure()
         else:
-            print("Trying to move down current figure")
+            # print("Trying to move down current figure")
             can_move = self.move_down()
             if can_move is False:
                 print("Cannot move figure anymore, fixing...")
-                self.fix_figure()
+                self._fix_figure()
+
+    def _get_full_row(self):
+        for y in range(self.height - 1, -1, -1):
+            is_full = True
+            for x in range(self.width):
+                if self._field[x][y].state != CellState.FILLED:
+                    is_full = False
+                    break
+            if is_full:
+                print("Full row:", y)
+                return y
+        return None
+
+    def _clear_rows(self):
+        while True:
+            row_index = self._get_full_row()
+            if row_index is None:
+                break
+
+            cells_to_destroy = [(x, row_index) for x in range(self.width)]
+            cells_to_move_down = []
+            for x in range(self.width):
+                for y in range(0, row_index):
+                    if self._field[x][y].state == CellState.FILLED:
+                        cells_to_move_down.append((x, y))
+
+            for x, y in cells_to_destroy:
+                self._set_cell_state(x, y, CellState.EMPTY)
+            self._refresh_ui()
+            time.sleep(self.tick_interval / 2)
+
+            for x, y in cells_to_move_down:
+                self._set_cell_state(x, y, CellState.EMPTY)
+            for x, y in cells_to_move_down:
+                self._set_cell_state(x, y + 1, CellState.FILLED)
+            time.sleep(self.tick_interval / 2)
+            self._refresh_ui()
