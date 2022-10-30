e5fea5bb991b7c122832767c9854eb0fe458b330
slafniy <slafniy@gmail.com>
2022-10-30 07:23:21 +0400

    Fixed build_windows.bat - use "py" launcher

--- build_windows.bat
+++ build_windows.bat
@@ -1,6 +1,6 @@
 :: Install dependencies for python
-pip install -r requirements.txt
+py -3 -m pip install -r requirements.txt
 
 :: Build EXE wrapper
-pyinstaller %~dp0tk_tetris.py --clean -F --specpath %~dp0\build --log-level ERROR --paths %~dp0\src ^
+py -3 -m PyInstaller %~dp0tk_tetris.py --clean -F --specpath %~dp0\build --log-level ERROR --paths %~dp0\src ^
 --add-data "%~dp0\res;res" --windowed
