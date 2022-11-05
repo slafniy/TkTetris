7a6d01edc587c847e50206c6a631298ce0243aac
slafniy <slafniy@gmail.com>
2022-11-05 16:31:40 +0400

    refactoring - extracted gfx resources to Skin class

--- app/tk_app.py
+++ app/tk_app.py
@@ -1,4 +1,5 @@
 import argparse
+import traceback
 import tkinter as tk
 import typing as t
 
@@ -6,9 +7,8 @@ import simpleaudio as sa
 import yaml
 
 import modules.abstract_ui as abstract_ui
-import modules.game as game
 import modules.controls_handler as ch
-
+import modules.game as game
 from modules.cell import CellState
 from modules.figures import Point
 from modules.logger import logger
@@ -17,6 +17,43 @@ from modules.resources import SoundResources, get_resources_path
 VERSION = '1.1d'
 
 COLOR_BACKGROUND = "#000000"
+
+
+class Skin:
+    def __init__(self, name: str):
+        self.sounds = SoundResources(name)  # Audio
+
+        gfx_resources_path = get_resources_path() / f'{name}' / 'gfx'
+
+        with (gfx_resources_path / "cfg.yaml").open() as yaml_file:
+            cfg = yaml.safe_load(yaml_file)
+
+        self.base_image = tk.PhotoImage(file=str(gfx_resources_path / "base.png"))
+
+        self.cell_size = cfg['cell_size']
+        self.game_field_offset_x = cfg['game_field_nw']['x']
+        self.game_field_offset_y = cfg['game_field_nw']['y']
+
+        self.next_figure_field_offset_x = cfg['next_figure_field_nw']['x']
+        self.next_figure_field_offset_y = cfg['next_figure_field_nw']['y']
+
+        self.cell_anchor_offset_x = cfg['cell_anchor_nw']['x']
+        self.cell_anchor_offset_y = cfg['cell_anchor_nw']['y']
+
+        self.cell_falling_image = tk.PhotoImage(file=str(gfx_resources_path / "cell_falling.png"))
+        self.cell_filled_image = tk.PhotoImage(file=str(gfx_resources_path / "cell_filled.png"))
+
+        self.pause_image = tk.PhotoImage(file=str(gfx_resources_path / "pause.png"))
+        self.pause_image_offset_x = cfg['pause_nw']['x']
+        self.pause_image_offset_y = cfg['pause_nw']['y']
+
+        # Scores
+        self.score_digit_offset_x = cfg['score_digit_nw']['x']
+        self.score_digit_offset_y = cfg['score_digit_nw']['y']
+        self.digit_width = cfg['digit_size']['width']
+        self.digit_height = cfg['digit_size']['height']
+        self.digit_images = {str(digit): tk.PhotoImage(file=str(gfx_resources_path / f"{digit}.png"))
+                             for digit in range(10)}
 
 
 class TkTetrisGUI(tk.Tk, abstract_ui.AbstractGUI):
@@ -33,26 +70,12 @@ class TkTetrisGUI(tk.Tk, abstract_ui.AbstractGUI):
 
         self._prepare_ui()  # initialize menus and binds
 
-        # Skin stuff
-        self._sounds: t.Optional[SoundResources] = None
-        self._base_canvas: t.Optional[tk.Canvas] = None
-        self._cell_falling_image: t.Optional[tk.PhotoImage] = None
-        self._cell_filled_image: t.Optional[tk.PhotoImage] = None
-        self._base_image: t.Optional[tk.PhotoImage] = None
-        self._cell_size: t.Optional[int] = None
-        self._cell_anchor_offset_x: t.Optional[int] = None
-        self._cell_anchor_offset_y: t.Optional[int] = None
-        self._game_field_offset_x: t.Optional[int] = None
-        self._next_figure_field_offset_x: t.Optional[int] = None
-        self._next_figure_field_offset_y: t.Optional[int] = None
-        self._game_field_offset_y: t.Optional[int] = None
-        self._pause_image: t.Optional[tk.PhotoImage] = None
-        self._pause_image_offset_x: t.Optional[int] = None
-        self._pause_image_offset_y: t.Optional[int] = None
         self._pause_image_id: t.Optional[int] = None  # to toggle pause
+        self._base_canvas: t.Optional[tk.Canvas] = None
         self._current_music: t.List[sa.PlayObject] = []
         self._current_skin_rb: tk.StringVar  # this is for radiobutton
         self._loaded_skin: t.Optional[str] = None  # this is to control loading skin if it's already loaded
+        self.skin: Skin
 
         self._next_figure_points: t.Set[Point] = set()  # store to repaint if skin changed
         self._next_figure_image_ids: t.Set[int] = set()
@@ -63,79 +86,55 @@ class TkTetrisGUI(tk.Tk, abstract_ui.AbstractGUI):
 
     @property
     def sounds(self) -> SoundResources:
-        return self._sounds
+        return self.skin.sounds
 
     def new_game(self):
         logger.info('Starting new game')
 
     def _load_skin(self, skin_name='Default'):
         """
-        Loads gfx and sounds from resources
+        Loads gfx and sounds from resources and applies them
         """
         if skin_name == self._loaded_skin:
             return
 
-        self._sounds = SoundResources(skin_name)  # Audio
+        try:
+            self.skin = Skin(skin_name)
+        except (KeyError, tk.TclError):
+            logger.error(f'Cannot load skin "{skin_name}!')
+            logger.debug(traceback.format_exc())
+            return  # Leave current skin unchanged
 
-        gfx_resources_path = get_resources_path() / f'{skin_name}' / 'gfx'
-
-        with (gfx_resources_path / "cfg.yaml").open() as yaml_file:
-            cfg = yaml.safe_load(yaml_file)
-
-        self._cell_size = cfg['cell_size']
-        self._game_field_offset_x = cfg['game_field_nw']['x']
-        self._game_field_offset_y = cfg['game_field_nw']['y']
-
-        self._next_figure_field_offset_x = cfg['next_figure_field_nw']['x']
-        self._next_figure_field_offset_y = cfg['next_figure_field_nw']['y']
-
-        self._cell_anchor_offset_x = cfg['cell_anchor_nw']['x']
-        self._cell_anchor_offset_y = cfg['cell_anchor_nw']['y']
-
-        self._cell_falling_image = tk.PhotoImage(file=str(gfx_resources_path / "cell_falling.png"))
-        self._cell_filled_image = tk.PhotoImage(file=str(gfx_resources_path / "cell_filled.png"))
-
-        self._pause_image = tk.PhotoImage(file=str(gfx_resources_path / "pause.png"))
-        self._pause_image_offset_x = cfg['pause_nw']['x']
-        self._pause_image_offset_y = cfg['pause_nw']['y']
-
-        self._base_image = tk.PhotoImage(file=str(gfx_resources_path / "base.png"))
         self._base_canvas = tk.Canvas(master=self,
-                                      width=self._base_image.width(),
-                                      height=self._base_image.height())
-        self._base_canvas.create_image(0, 0, image=self._base_image, anchor=tk.NW)
+                                      width=self.skin.base_image.width(),
+                                      height=self.skin.base_image.height())
+        self._base_canvas.create_image(0, 0, image=self.skin.base_image, anchor=tk.NW)
         self._base_canvas.grid(column=0, row=0, sticky=tk.NW)
-        self.geometry(f'{self._base_image.width()}x{self._base_image.height()}')
+        self.geometry(f'{self.skin.base_image.width()}x{self.skin.base_image.height()}')
 
         # Scores
-        self._score_digit_offset_x = cfg['score_digit_nw']['x']
-        self._score_digit_offset_y = cfg['score_digit_nw']['y']
-        self._digit_width = cfg['digit_size']['width']
-        self._digit_height = cfg['digit_size']['height']
-        self._digit_images = {str(digit): tk.PhotoImage(file=str(gfx_resources_path / f"{digit}.png"))
-                              for digit in range(10)}
         self.show_score(0)
 
-        # Stop any music if any and run new
-        [i.stop() for i in self._current_music]
-        # self._current_music.append(self.sounds.startup.play())
+        # Stop any music
+        for i in self._current_music:
+            i.stop()
         self._loaded_skin = skin_name
 
         # Repaint stuff if any
         self.show_next_figure(self._next_figure_points)
         for point, cell in self._game_field_cells.items():
             if cell[1] == CellState.FILLED:
-                self._paint_cell(point, self._cell_filled_image)
+                self._paint_cell(point, self.skin.cell_filled_image)
 
     def show_next_figure(self, points: t.Set[Point]):
         self._next_figure_points = points
         [self.delete_image(i) for i in self._next_figure_image_ids]
         self._next_figure_image_ids = set()
         for x, y in points:
-            _x = x * self._cell_size + self._next_figure_field_offset_x - self._cell_anchor_offset_x
-            _y = y * self._cell_size + self._next_figure_field_offset_y - self._cell_anchor_offset_y
+            _x = x * self.skin.cell_size + self.skin.next_figure_field_offset_x - self.skin.cell_anchor_offset_x
+            _y = y * self.skin.cell_size + self.skin.next_figure_field_offset_y - self.skin.cell_anchor_offset_y
             self._next_figure_image_ids.add(
-                self._base_canvas.create_image(_x, _y, anchor=tk.NW, image=self._cell_falling_image))
+                self._base_canvas.create_image(_x, _y, anchor=tk.NW, image=self.skin.cell_falling_image))
 
     def refresh_ui(self):
         self._base_canvas.update()
@@ -147,13 +146,13 @@ class TkTetrisGUI(tk.Tk, abstract_ui.AbstractGUI):
         [self.delete_image(i) for i in self._score_image_ids]
         score_str = f'{score:04d}'
         for i, digit in enumerate(score_str):
-            x = self._score_digit_offset_x + i * self._digit_width
-            self._score_image_ids.add(self._base_canvas.create_image(x, self._score_digit_offset_y,
-                                                                     anchor=tk.NW, image=self._digit_images[digit]))
+            x = self.skin.score_digit_offset_x + i * self.skin.digit_width
+            self._score_image_ids.add(self._base_canvas.create_image(x, self.skin.score_digit_offset_y,
+                                                                     anchor=tk.NW, image=self.skin.digit_images[digit]))
 
     def _paint_cell(self, point: Point, cell_image: tk.PhotoImage) -> int:
-        x = point.x * self._cell_size + self._game_field_offset_x - self._cell_anchor_offset_x
-        y = point.y * self._cell_size + self._game_field_offset_y - self._cell_anchor_offset_y
+        x = point.x * self.skin.cell_size + self.skin.game_field_offset_x - self.skin.cell_anchor_offset_x
+        y = point.y * self.skin.cell_size + self.skin.game_field_offset_y - self.skin.cell_anchor_offset_y
         return self._base_canvas.create_image(x, y, anchor=tk.NW, image=cell_image)
 
     def apply_field_change(self, changed_points: t.OrderedDict[CellState, t.Set[Point]]):
@@ -170,7 +169,7 @@ class TkTetrisGUI(tk.Tk, abstract_ui.AbstractGUI):
                 self.delete_image(image_id)
 
     def _paint_cells(self, points: t.Set[Point], state: CellState):
-        cell_image = self._cell_falling_image if state == CellState.FALLING else self._cell_filled_image
+        cell_image = self.skin.cell_falling_image if state == CellState.FALLING else self.skin.cell_filled_image
         for point in points:
             self._game_field_cells[point] = (self._paint_cell(point, cell_image), state)
 
@@ -182,9 +181,9 @@ class TkTetrisGUI(tk.Tk, abstract_ui.AbstractGUI):
             self.delete_image(self._pause_image_id)
             self._pause_image_id = None
         else:
-            self._pause_image_id = self._base_canvas.create_image(self._pause_image_offset_x,
-                                                                  self._pause_image_offset_y,
-                                                                  anchor=tk.NW, image=self._pause_image)
+            self._pause_image_id = self._base_canvas.create_image(self.skin.pause_image_offset_x,
+                                                                  self.skin.pause_image_offset_y,
+                                                                  anchor=tk.NW, image=self.skin.pause_image)
 
     def _prepare_ui(self):
 
@@ -221,17 +220,16 @@ def main():
     controls_handler = ch.ControlsHandler()
 
     # Create main GUI class and bind controls handler to it
-    ui_root = TkTetrisGUI()
-    ui_root.bind(sequence='<KeyPress>', func=controls_handler.on_key_press)
-    ui_root.bind(sequence='<KeyRelease>', func=controls_handler.on_key_release)
+    gui = TkTetrisGUI()
+    gui.bind(sequence='<KeyPress>', func=controls_handler.on_key_press)
+    gui.bind(sequence='<KeyRelease>', func=controls_handler.on_key_release)
+    gui.geometry("+800+300")
 
     # Game logic class - binds GUI, controls and logic together
-    game.Game(controls_handler=controls_handler, gui=ui_root)
-
-    ui_root.geometry("+800+300")
+    game.Game(controls_handler=controls_handler, gui=gui)
 
     # Start application
-    ui_root.mainloop()
+    gui.mainloop()
 
 
 if __name__ == "__main__":
